05e8a636ad54b3c608dacfd2e0a35ed1cba0b7d9
DennyPlank <DennyMouzon@gmail.com>
2022-01-04 15:22:24 -0800

    seeds file and models working

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,37 @@
 # DatabaseCleaner.strategy = :truncation
 
 require 'faker'
+catagories = [
+  'art',
+  'sports',
+  'food',
+]
+
+10.times do
+  a = Seller.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+
+  2.times do
+    m_price = rand(50..1000);
+    Buyer.create(
+      name: Faker::Name.name,
+      max_price: m_price,
+      desired_catagories: catagories.sample,
+      seller_id: a.id
+    )
+  end
+  
+  10.times do
+    price = rand(50..1000)
+    Product.create(
+      price: price,
+      description: Faker::Quote,
+      category: catagories.sample,
+      seller_id: a.id
+  )
+  end
+end
+
+puts '10 sellers, 20 buyers, 100 products seed'
